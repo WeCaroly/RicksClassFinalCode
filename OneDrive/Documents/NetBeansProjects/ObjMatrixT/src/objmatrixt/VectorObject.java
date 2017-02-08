fab0bae73d9475ec03da319c8fffb3840d65a4db
carol_8wybosj <carol_8wybosj@10.34.6.102>
2017-02-07 20:42:17 -0600

    started broken

--- OneDrive/Documents/NetBeansProjects/ObjMatrixT/src/objmatrixt/VectorObject.java
+++ OneDrive/Documents/NetBeansProjects/ObjMatrixT/src/objmatrixt/VectorObject.java
@@ -14,84 +14,77 @@ import javagames.util.Vector2f;
  * @author carol_8wybosj
  */
 public class VectorObject implements Drawable{
-	public Vector2f[] objects;
-        public Matrix3x3f[] world;
+	public Vector2f[] lines;
+        public Matrix3x3f world;
         Point point = new Point(0,0);
-        int location;
-        int scale;
+        Point location;
+        Float scale;
         Color color;    
-        int rotation;
+        float rotation = 0.2f;
         //Added
         int widthScreen, heightScreen;
         int widthScreenNeg, heightScreenNeg;
-        
-        
 
-    VectorObject(int i) {
-        
+    VectorObject(int i, Color color) {
+        world = Matrix3x3f.identity();
+        setColor(color);
         switch(i){
             case 1: //tri
-                objects = new Vector2f[] { new Vector2f(10, 0), new Vector2f(0, 10),
-				new Vector2f(-10, 0), new Vector2f(0, -10), };
+                lines = new Vector2f[] { new Vector2f(10, 0), new Vector2f(0, 10),
+				new Vector2f(0, 0) };
                 break;
             case 2: // hex
-                objects = new Vector2f[] { new Vector2f(10, 0), new Vector2f(0, 10),
-				new Vector2f(-10, 0), new Vector2f(0, -10), };
+                lines = new Vector2f[] { new Vector2f(0, 20), new Vector2f(0, 10),
+				new Vector2f(10, 0), new Vector2f(20, 10), 
+                                new Vector2f(20, 20), new Vector2f(10, 20) };
                 break;
             case 3: //square
-                objects = new Vector2f[] { new Vector2f(10, 0), new Vector2f(0, 10),
-				new Vector2f(-10, 0), new Vector2f(0, -10), };
+                lines = new Vector2f[] { new Vector2f(10, 0), new Vector2f(0, 10),
+				new Vector2f(-10, 0), new Vector2f(0, -10) };
                 break;
         }
      }
         
     @Override
     public void updateWorld() {
-    
-      //Two transgormations 
-        // move and multiply
-      
+        //multiplys the world matrix by the rotation value set
+        world = world.mul(Matrix3x3f.rotate(rotation));
+        //sclaes the world by the sale set 
+        world = Matrix3x3f.scale(scale,scale);
+        //translated vector to matrix obj
+        Matrix3x3f temp = Matrix3x3f.translate(new Vector2f(location.x,location.y));
+        world = world.mul(temp);
     }
 
     @Override
     public void render(Graphics g) {
-       
-        Vector2f S;
-        //S = world[world.length - 1];
-        Vector2f P = null;
-        //adds to screen
-        for (int i = 0; i < world.length; ++i) {
-         //  P = world[i];
-         //   g.drawLine((int) S.x, (int) S.y, (int) P.x, (int) P.y);
-            S = P;
-	}        
+        g.setColor(color);
+        
+        for(int x = 0; x < lines.length-1; x++){
+            g.drawLine((int)world.mul(lines[x]).x,(int)world.mul(lines[x]).y, 
+        			(int)world.mul(lines[x+1]).x, (int)world.mul(lines[x+1]).y);
+        }
+        g.drawLine((int)world.mul(lines[lines.length-1]).x, (int)world.mul(lines[lines.length-1]).y, 
+            (int)world.mul(lines[0]).x, (int)world.mul(lines[0]).y);
         
        
-        g.drawLine(point.x + 10, point.y, point.x, point.y);
-        g.drawLine(point.x, point.y + 10, point.x, point.y);
-        g.drawLine(point.x, point.y, point.x - 10, point.y);
-        g.drawLine(point.x, point.y, point.x, point.y - 10);
-
-        
-        
-         /* draw the sun...
-        Matrix3x3f sunMat = Matrix3x3f.identity();
-        sunMat = sunMat.mul(Matrix3x3f.translate(1280 / 2, 720 / 2));
-        Vector2f sun = sunMat.mul(new Vector2f());
-        */
+//        g.drawLine(point.x + 10, point.y, point.x, point.y);
+//        g.drawLine(point.x, point.y + 10, point.x, point.y);
+//        g.drawLine(point.x, point.y, point.x - 10, point.y);
+//        g.drawLine(point.x, point.y, point.x, point.y - 10);
     }
     
-    public void move(float x, float y){
-        if(objects != null){
-            if((x < widthScreen && x > widthScreenNeg)
-                    &&(y < widthScreen && y > widthScreenNeg)){
-                //TODO: CHECK CORRECTNESS
-                for(int i = 0; i < objects.length;++i){
-                    objects[i].translate(x, y);
-                }
-            } 
-        }
-    }
+//    public void move(float x, float y){
+//        if(lines != null){
+//            if((x < widthScreen && x > widthScreenNeg)
+//                    &&(y < widthScreen && y > widthScreenNeg)){
+//                //TODO: CHECK CORRECTNESS
+//                for(int i = 0; i < lines.length;++i){
+//                    lines[i].translate(x, y);
+//                }
+//            } 
+//        }
+//    }
     public void updatePoint(Point point){
         this.point = point;
     }
@@ -102,11 +95,11 @@ public class VectorObject implements Drawable{
         widthScreenNeg = w - (w+w);
     }
     
-    public void setWord(Matrix3x3f[] world){
+    public void setWord(Matrix3x3f world){
         this.world = world;
     }
     
-    public void setLocation(int location){
+    public void setLocation(Point location){
         this.location = location;
     }
     
@@ -114,10 +107,10 @@ public class VectorObject implements Drawable{
         this.color = color;
     }
     
-    public void setRotation(int rotation){
+    public void setRotation(float rotation){
            this.rotation = rotation;
     }
-    public void setScale(int scale){
+    public void setScale(float scale){
         this.scale = scale;
     }
 }
