815aa372d8df26807679ec48e088497984e87748
carol_8wybosj <carol_8wybosj@10.34.6.102>
2017-02-25 14:42:44 -0600

    pariatal fix of meteors

--- OneDrive/Documents/NetBeansProjects/CannonCommand/src/javagames/util/CityBlockManager.java
+++ OneDrive/Documents/NetBeansProjects/CannonCommand/src/javagames/util/CityBlockManager.java
@@ -5,10 +5,32 @@
  */
 package javagames.util;
 
+import static java.awt.Color.BLUE;
+import java.util.ArrayList;
+import javagame.VectorObject;
+
 /**
  *
  * @author carol_8wybosj
  */
 public class CityBlockManager {
+    public VectorObject city[];
     
+    public void removebuilding(int i){
+        city[i] = null;
+    }
+    
+    public void cityMaker(){
+        ArrayList<VectorObject> cityBlock = null;
+        VectorObject vector;
+        for(int x = 0;x<15;x++){
+            vector = new VectorObject(4,BLUE);
+        cityBlock.add(vector);
+        }
+    }
+    
+    public boolean hitBuilding(int vectorObject){
+        //remove the vectorObject from the city if it is destroyed
+        return true;
+    }  
 }
