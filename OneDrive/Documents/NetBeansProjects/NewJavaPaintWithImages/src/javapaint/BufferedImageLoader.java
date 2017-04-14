f1ca0564ea5e20bcb6e75369a949287d1bc77edd
carol_8wybosj <carol_8wybosj@10.34.6.102>
2017-04-13 23:03:15 -0500

    Update 3.1

---
+++ OneDrive/Documents/NetBeansProjects/NewJavaPaintWithImages/src/javapaint/BufferedImageLoader.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class BufferedImageLoader {
+
+	public BufferedImage img;
+	
+	public BufferedImage loadImage (String path) throws IOException{
+		
+		img = ImageIO.read(getClass().getResourceAsStream(path));
+		return img;
+	}
+}
